fcbf4a131717a919bed9ff850ad835c0490c2067
Rafael Cruz <rafac@Rafaels-MacBook-Pro.local>
2018-10-12 12:55:55 -0400

    added styling

--- app/src/App.js
+++ app/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import NavBar from "./components/Navbar/NavBar";
 import Search from "./components/search/Search";
+import "./App.css";
 
 export default class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div>
+        <div className="container">
           <NavBar />
           <Search />
         </div>
